21dfa3a00131d88859695a7fc879bc46d4548cf2
nadri.k <nadri.k@VDI-DOTNET2-18>
2024-03-11 08:44:26 +0330

    Completed Blazor - Data and Property Binding

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsActive { get; set; }
-
+        public double Price { get; set; }
+        public List<Demo_ProductProp> ProductProperties { get; set; }
     }
 }
